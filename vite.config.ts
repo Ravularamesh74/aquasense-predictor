f9a12988f33f0fb8b3033170c7acbdd3c5f86fe0
Ravula Ramesh <ravularamesh74@gmail.com>
2026-04-17 20:04:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,3 +19,6 @@ export default defineConfig(({ mode }) => ({
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 }));
+
+
+
